be5a9820f918085d52ce11d3ce59b2a9541f56fa
matheus-oliveira-andrade <mathnhais@outlook.com>
2024-02-29 21:37:01 -0300

    feat: initial changes

--- build-push.sh
+++ build-push.sh
@@ -5,7 +5,7 @@ docker login
 
 
 # build and push fluentd docker image
-docker build -t micrommath/fluentd ./fluentd
+docker build -t micrommath/fluentd ./applications/fluentd
 
 docker image push micrommath/fluentd
 
@@ -13,8 +13,8 @@ docker image push micrommath/fluentd
 # build and push transactions movement api docker image
 docker build \
   --tag micrommath/transaction-movements-api \
-  --file ./transactions-movements-app/Dockerfile.Api \
-  ./transactions-movements-app
+  --file ./applications/transactions-movements-app/Dockerfile.Api \
+  ./applications/transactions-movements-app
 
 docker image push micrommath/transaction-movements-api
 
@@ -22,8 +22,8 @@ docker image push micrommath/transaction-movements-api
 # build and push transactions movement async receiver docker image
 docker build \
   --tag micrommath/transaction-movements-async-receiver \
-  --file ./transactions-movements-app/Dockerfile.AsyncReceiver \
-  ./transactions-movements-app
+  --file ./applications/transactions-movements-app/Dockerfile.AsyncReceiver \
+  ./applications/transactions-movements-app
 
 docker image push micrommath/transaction-movements-async-receiver
 
@@ -31,6 +31,6 @@ docker image push micrommath/transaction-movements-async-receiver
 # build and push transactions seed docker image
 docker build \
   --tag micrommath/transactions-seed \
-  ./transactions-seed-app
+  ./applications/transactions-seed-app
 
 docker image push micrommath/transactions-seed
